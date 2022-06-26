30369cc6a2f0643d2bdef4be65bfcb383ab7b37c
skwazz <skwazz@gmail.com>
2022-06-26 13:43:08 +0300

    Daily of Week 3 day 2

---
+++ Week3/Day2/Daily/main.js
@@ -0,0 +1,41 @@
+// Exercise 1:
+
+// Task 1.1:
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+fruits.shift();
+
+console.log(fruits);
+
+// Task 1.2:
+
+fruits.sort();
+
+console.log(fruits);
+
+// Task 1.3:
+
+fruits.push('Kiwi');
+
+console.log(fruits);
+
+// Task 1.4:
+
+fruits.splice(0, 1, );
+
+console.log(fruits);
+
+// Task 1.5: 
+
+fruits.reverse();
+
+console.log(fruits);
+
+// Exercise 2:
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+let moreFruitsOrange = moreFruits[1][1];
+
+console.log(moreFruitsOrange);
