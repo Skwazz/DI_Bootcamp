f6380a504a55135658bc597fdadc7bd94da27830
skwazz <skwazz@gmail.com>
2022-07-04 00:33:24 +0300

    added Day 4 daily challange

--- Week3/Day4/Daily/main.js
+++ Week3/Day4/Daily/main.js
@@ -1,70 +1,17 @@
-// Exercise 1: Simple If/Else Statement : 
+let sentence = `The movie is not that bad, I like it`;
 
-let x = 5;
-let y = 2;
+// let wordNot = sentence.indexOf('not')
+// let wordBad = sentence.indexOf('bad')
 
-function checkBigger() {
-    if (x > y) {
-        console.log(`x is the biggest number`);
-    } else {
-        console.log(`y is the biggest number`);
-    }
-}
+// let newSentace = sentence
+// console.log(wordNot);
+// console.log(wordBad);
 
-checkBigger();
+// if (wordNot < wordBad && wordNot !== -1) {
+//    newSentace = `${sentence.substring(0,wordNot)} good ${sentence.substring(wordBad + 3)}`
+// }
+// console.log(newSentace);
 
-//  Exercise 2: Chihuahua :
+sentence.replace(/not.*bad/,'good')
 
-let newDog = `Chihuahua`;
-
-console.log(newDog.length);
-console.log(newDog.toUpperCase());
-console.log(newDog.toLowerCase());
-
-if (newDog = `Chihuahua`) {
-    console.log(`I love Chihuahuas, it's my favorite dog breed`);
-} else {
-    console.log(`I dont care, I prefer cats`);
-}
-
-
-// Exercise 3: Even Or Odd :
-
- let userChoice = prompt(`Check if your number is even or odd : `)
-
-function evenOrOdd() {
-    if (userChoice % 2 == 0) {
-        console.log(`${userChoice} is an even number!`);
-    } else 
-        console.log(`${userChoice} is an odd number!`);
-    }
-
-    evenOrOdd();
-
-
-    // Exercise 4: Group Chat : 
-
-    let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
-
-    // function whoIsOnline() {
-    //     switch (users.length) {
-    //         case users.length = 0:
-    //             console.log(`no one is online`);
-    //             break;
-    //         case users.length = 1:
-    //             console.log(`${users.indexOf(0)} is online`);
-    //             break;    
-    //         case users.length = 2:
-    //             console.log(`${users.indexOf(0)} and ${users.indexOf(1)} is online`);
-    //             break;     
-    //         case users.length > 2:
-    //             console.log(`${users.indexOf(0)},${users.indexOf(1)} and ${users.length()} more are online `);
-    //             break;     
-    //             default:
-
-
-    //             break;
-    //     }
-    // }
-
-    // whoIsOnline();
+console.log(sentence);
