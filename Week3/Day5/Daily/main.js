750100208b492e61b97a3c7c4e3ac52128fb919c
skwazz <skwazz@gmail.com>
2022-07-03 20:36:58 +0300

    better version

--- Week3/Day5/Daily/main.js
+++ Week3/Day5/Daily/main.js
@@ -1,17 +1,21 @@
-let pyramid = {
-    firstStar:  '*',
-    secondStar: '**',
-    thirdStar:  '***',
-    fourthStar: '****',
-    fifthStar:  '*****',
-    sixthStar:  '******',
-}
+let toPrint = ''
 
-for (const key in pyramid) {
-    if (Object.hasOwnProperty.call(pyramid, key)) {
-        const element = pyramid[key];
-        console.log(element);
-    }
+for (let index = 1; index < 6; index++) {
+    toPrint += "*".repeat(index)
+    toPrint += "\n"
+    
 }
+console.log(toPrint);
 
 // Best I could do^
+
+for (let index = 1; index <= 6; index++) {
+    let toPrint2 = ''
+    for (let index2 = 1; index2 <= index; index2++) {
+        toPrint2+= '*'
+        
+    }
+    toPrint2+= '\n'
+    console.log(toPrint2);
+}
+
