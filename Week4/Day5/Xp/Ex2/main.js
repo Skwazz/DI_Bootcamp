de424f135a96bdc16a90b9d06f692f7003d5b4a8
skwazz <skwazz@gmail.com>
2022-07-10 12:02:29 +0300

    ex2

---
+++ Week4/Day5/Xp/Ex2/main.js
@@ -0,0 +1,15 @@
+let addBlue = document.querySelector(`div`);
+
+addBlue.style.backgroundColor = `lightblue`;
+
+let displayJohn = document.querySelector(`li`);
+
+displayJohn.style.display = `none`;
+
+let borderPete = document.getElementsByTagName(`li`)[1];
+
+borderPete.style.border = `1px black solid`;
+
+let fontOfWholeBody = document.querySelector(`body`);
+
+fontOfWholeBody.style.fontSize = `30px`;
