ad933b08aae17fd210a0480397c051458c8ac8f5
skwazz <skwazz@gmail.com>
2022-07-10 12:20:16 +0300

    ex3

---
+++ Week4/Day5/Xp/Ex3/main.js
@@ -0,0 +1,14 @@
+// 3.2
+
+let navBar = document.querySelector(`#navBar`);
+
+navBar.setAttribute(`id`, `socialNetworkNavigation`)
+
+// 3.3
+let getUl = document.querySelector(`ul`);
+let addLi = document.createElement(`li`);
+
+
+console.log(addLi);
+
+getUl.appendChild(addLi);
