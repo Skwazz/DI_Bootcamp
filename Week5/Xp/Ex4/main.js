237470f52b1ca8854e423d9d13d0927036c8d680
skwazz <skwazz@gmail.com>
2022-08-03 22:54:44 +0300

    week 6 day 2 ex 1

--- Week5/Xp/Ex4/main.js
+++
@@ -1,8 +0,0 @@
-let userInput = document.forms[0];
-userInput.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let r = userInput.radius.value;
-  let volume = userInput.volume.value;
-  userInput.radius.value = "";
-  userInput.volume.value = (4 / 3) * 3.1415926535 * (r * r * r);
-});
