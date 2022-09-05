8daeedf232c294539234e931f05b93186673c15a
skwazz <skwazz@gmail.com>
2022-09-05 23:17:04 +0300

    week 12 day 2 xp

---
+++ Week12/Day2/Xp/Ex3/main.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+let x;
+let c = 6;
+x = a + b;
+let sum = x + c;
+console.log('first run', x);
+console.log("Second run", sum);
---
+++ Week12/Day2/Xp/npm-node/main.js
@@ -0,0 +1,11 @@
+// Exercise I:
+
+let a = 5;
+let b = 10;
+
+let age = `Hello you are ${a+b}`
+
+console.log(age);
+
+// Exercise II :
+console.log(`Hi, thanks for running me with npm start`);
