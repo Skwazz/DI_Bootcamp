4b5cf67ceae62b80092f391d723b9f6997b1b201
skwazz <skwazz@gmail.com>
2022-09-23 20:50:36 +0300

    week14 day 2 daily

---
+++ Week14/Day2/Daily/carousel/src/slider.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+function Slider() {
+  return (
+    <div style={{width:'500px', margin:'0 auto', height:''}}>
+      <Carousel>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg"  />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp"  />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp"  />
+        </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp"  />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default Slider;
